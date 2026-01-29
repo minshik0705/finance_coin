943799e52b464487563662f84fa375ffaf6ce3fc
minshik0705 <112872144+minshik0705@users.noreply.github.com>
2026-01-29 20:39:16 +0900

    프로젝트 2일차 모듈 설치 해결

--- get_data.py
+++ get_data.py
@@ -1,22 +1,29 @@
-import config, csv
+import csv
 from binance.client import Client
+import config
 
 client = Client(config.API_KEY, config.API_SECRET)
 
-prices = client.get_all_tickers()
+SYMBOL   = "BTCUSDT"                      
+INTERVAL = Client.KLINE_INTERVAL_5MINUTE
+START    = "1 Jan, 2020"
+END      = "1 Jan, 2025"
 
-#for price in prices:
-#    print(price)
+try:
+    rows = client.get_historical_klines(SYMBOL, INTERVAL, START, END)
+    print(f"Fetched {len(rows)} rows")
 
-candles = client.get_klines(symbol='BTCUSDT', interval=Client.KLINE_INTERVAL_15MINUTE)
-
-csvfile = open('15minutes.csv', 'w', newline='')
-
-candlestick_writer = csv.writer(csvfile, delimiter=',')
-
-for candlestick in candles:
-    print(candlestick)
-
-    candlestick_writer.writerow(candlestick)
-
-print(len(candles))
+    if not rows:
+        print("No data returned. Check symbol/interval/date range.")
+    else:
+        with open("2020-2025.csv", "w", newline="") as f:
+            w = csv.writer(f)
+            w.writerow([
+                "Open time","Open","High","Low","Close","Volume",
+                "Close time","Quote asset volume","Number of trades",
+                "Taker buy base asset volume","Taker buy quote asset volume","Ignore"
+            ])
+            w.writerows(rows)
+        print("Wrote 2020-2025.csv")
+except Exception as e:
+    print("Error:", e)
